38cafff7bb4140e6cd2ecd2da4f8e6179f0f75aa
AlyAbdelrahman <aly.m.abdelrahman@gmail.com>
2023-08-30 23:24:01 +0300

    adding i18n translation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {HttpClientModule} from '@angular/common/http'
+import {HttpClientModule, HttpClient} from '@angular/common/http'
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './core/navbar/navbar.component';
@@ -9,6 +9,11 @@ import { CardComponent } from './features/card/card.component';
 import { FormsModule } from '@angular/forms';
 import { DateCardComponent } from './features/date-card/date-card.component';
 import { SearchComponent } from './features/search/search.component';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
 
 @NgModule({
   declarations: [
@@ -22,7 +27,14 @@ import { SearchComponent } from './features/search/search.component';
   imports: [
     BrowserModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-home',
@@ -6,16 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
-  dafaultLang: string = 'EN';
-
+  constructor(private translate: TranslateService){
+    
+  }
   switchlang(){
     let lang =  document.getElementById('lang');
     if (lang && lang.getAttribute('href') == 'assets/css/style-ltr.css') {
       lang.setAttribute('href', 'assets/css/style-rtl.css');
-      this.dafaultLang = 'EN'
+      this.translate.use('ar');
   } else {
     lang && lang.setAttribute('href', 'assets/css/style-ltr.css');
-    this.dafaultLang = 'AR'
+    this.translate.use('en');
 
   }
   }
