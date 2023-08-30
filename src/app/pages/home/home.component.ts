126885e6986b56a7591a85945907b6c4722a3b0d
AlyAbdelrahman <aly.m.abdelrahman@gmail.com>
2023-08-30 20:55:07 +0300

    add switch lang button

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
+  dafaultLang: string = 'EN';
 
+  switchlang(){
+    let lang =  document.getElementById('lang');
+    if (lang && lang.getAttribute('href') == 'assets/css/style-ltr.css') {
+      lang.setAttribute('href', 'assets/css/style-rtl.css');
+      this.dafaultLang = 'EN'
+  } else {
+    lang && lang.setAttribute('href', 'assets/css/style-ltr.css');
+    this.dafaultLang = 'AR'
+
+  }
+  }
 }
